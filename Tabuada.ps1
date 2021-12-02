452aae16c07d26132055bed883a9f172f11a0ba3
User User <useruser1985official@outlook.com>
2021-12-02 15:57:10 -0300

    Primeiro Commit

---
+++ Tabuada.ps1
@@ -0,0 +1,29 @@
+﻿function tabuada() {
+    $num = [int](Read-Host "Digite um número")
+
+    "`n"
+
+    "Tabuada de $num`:"
+
+    "`n"
+
+    "-----------------"
+
+    for($t = 1; $t -le 10; $t++) {
+        $res = $num * $t
+
+        "$num x $t = $res"
+    }
+
+    "-----------------"
+
+    $opc = Read-Host "Deseja digitar outros números? [S/N]"
+
+    if($opc -eq 's') {
+        Clear-Host
+
+        tabuada
+    }
+}
+
+tabuada
---
+++ Área Terreno.ps1
@@ -0,0 +1,13 @@
+﻿function area($larg, $comp) {
+    $a = $larg * $comp
+
+    "A área de um terreno $larg x $comp é de $a`M²."
+}
+
+"Controle de Terrenos"
+"-" * 20
+
+$l = [float](Read-Host "Largura (m)")
+$c = [float](Read-Host "Altura (m)")
+
+area $l $c
